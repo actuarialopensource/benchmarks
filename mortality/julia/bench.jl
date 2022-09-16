02dabcedc0f48b56a72139b451ec4542912dac9b
Matthew Caseres <matthewcaseres@outlook.com>
2022-09-16 16:28:21 -0500

    fmt

--- mortality/julia/bench.jl
+++ mortality/julia/bench.jl
@@ -2,16 +2,16 @@ using MortalityTables
 using BenchmarkTools
 
 function runner()
-    tbls = [MortalityTables.table(i) for i in 3299:3308];
+    tbls = [MortalityTables.table(i) for i in 3299:3308]
     issue_ages = 18:50
     durations = 1:25
     timesteps = 0:29
     q = [
-        tbl.select[issue_age][issue_age+(duration-1) + timestep]
+        tbl.select[issue_age][issue_age+(duration-1)+timestep]
         for timestep in timesteps, (tbl, issue_age, duration) in vec(collect(Iterators.product(tbls, issue_ages, durations)))
     ]
     npx = [1; cumprod(1 .- q[begin:end-1, :], dims=1)]
-    v = 1/1.02
+    v = 1 / 1.02
     v_eoy = v .^ (1 .+ timesteps) # this broadcasts
     unit_claims_discounted = npx .* q .* v_eoy
     sum(unit_claims_discounted)
