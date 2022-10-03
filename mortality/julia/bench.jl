5eadb404d641a462b1b9babc09108d00ce35df98
Matthew Caseres <matthewcaseres@outlook.com>
2022-10-03 00:20:41 -0500

    faster codes

--- mortality/julia/bench.jl
+++ mortality/julia/bench.jl
@@ -1,23 +1,32 @@
 using MortalityTables
 using BenchmarkTools
 
-function runner()
-    tbls = [MortalityTables.table(i) for i in 3299:3308]
-    issue_ages = 18:50
-    durations = 1:25
-    timesteps = 0:29
-    q = [
-        tbl.select[issue_age][issue_age+(duration-1)+timestep]
-        for timestep in timesteps, (tbl, issue_age, duration) in vec(collect(Iterators.product(tbls, issue_ages, durations)))
-    ]
-    npx = [1; cumprod(1 .- q[begin:end-1, :], dims=1)]
-    v = 1 / 1.02
-    v_eoy = v .^ (1 .+ timesteps) # this broadcasts
-    unit_claims_discounted = npx .* q .* v_eoy
-    sum(unit_claims_discounted)
+@inline function npv(qs, r, term=length(qs))
+    inforce, result = 1.0, 0.0
+    v = 1 / (1 + r)
+    v_t = v
+    @inbounds @simd for t in 1:min(term, length(qs))
+        q = qs[t]
+        result += inforce * q * v_t
+        inforce = inforce * (1 - q)
+        v_t *= v
+    end
+    return result
 end
 
-@assert runner() == 1904.486552663679
+function runner(tbls=[MortalityTables.table(i) for i in 3299:3308])
+    issue_ages = 18:50
+    durations = 1:25
+    term = 29
+    total = 0.0
+    @inbounds for i in eachindex(tbls), ia in issue_ages, dur in durations
+        start_age = ia + dur - 1
+        total += @views npv(tbls[i].select[ia][start_age:start_age+term], 0.02)
+    end
+    return total
+end
+
+@assert runner() ≈ 1904.486552663679
 
 b = @benchmark runner()
 
