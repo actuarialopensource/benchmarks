1c8b5324fa8ee7998cb362952817792047051e01
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-16 18:30:14 -0500

    clear cache between calls when benchmarking

--- Julia/basic_term.jl
+++ Julia/basic_term.jl
@@ -1,7 +1,13 @@
 import CacheFlow as cf
 
+function cf1()
+    empty!(cf.cache_policies_inforce)
+    empty!(cf.cache_premiums_pp)
+    cf.result_cf()
+end
+
 function run_basic_term_benchmark()
-    cf1_benchmark = @benchmark cf.result_cf()
+    cf1_benchmark = @benchmark cf1()
     # (result, mean time, median time) named tuple
     return Dict(
         "cf1" => Dict(
