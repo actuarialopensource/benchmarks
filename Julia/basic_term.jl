b20de231da7dddee364393261967270b8f2a5978
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-16 18:05:40 -0500

    Try to fix Julia formatting output error in README

--- Julia/basic_term.jl
+++ Julia/basic_term.jl
@@ -1,12 +1,10 @@
 import CacheFlow as cf
 
 function run_basic_term_benchmark()
-    cf1_result = cf.result_cf()
     cf1_benchmark = @benchmark cf.result_cf()
     # (result, mean time, median time) named tuple
     return Dict(
         "cf1" => Dict(
-            "result" => cf1_result[[1, 240], :],
             "mean" => string(mean(cf1_benchmark)),
         ),
     )
