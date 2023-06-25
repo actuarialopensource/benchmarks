13c7e43f8b7ab0155f245d4095dbada2d843dd23
serenity4 <cedric.bel@hotmail.fr>
2023-06-25 23:39:11 +0200

    Rename Python benchmark section

--- Python/savings_me_lifelib.py
+++ Python/savings_me_lifelib.py
@@ -4,17 +4,17 @@ import timeit
 
 m = mx.read_model("CashValue_ME_EX4")
 
-def savings_se_lifelib():
+def savings_me_lifelib():
     m.Projection.clear_cache = 1
     m.Projection.scen_size = 1
     return float(np.sum(m.Projection.pv_net_cf()))
 
 def run_savings_benchmarks():
     trials = 20
-    modelx_time = timeit.timeit(stmt="savings_se_lifelib()", number=trials, globals = {"savings_se_lifelib": savings_se_lifelib})
-    modelx_result = savings_se_lifelib()
+    modelx_time = timeit.timeit(stmt="savings_me_lifelib()", number=trials, globals = {"savings_me_lifelib": savings_me_lifelib})
+    modelx_result = savings_me_lifelib()
     return {
-        "Python lifelib basic_term_m": {
+        "Python lifelib cashvalue_me_ex4": {
             "mean": f"{(modelx_time / trials)*1000} milliseconds",
             "result": modelx_result,
         }
